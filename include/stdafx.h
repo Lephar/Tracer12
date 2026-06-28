a102e2a960345502c0d4b6667f71f4d781c87b9f
Ali Emre Gülcü <aliemreglc@gmail.com>
2026-06-28 15:24:31 +0300

    Add dependencies and create precompiled header

--- include/stdafx.h
+++ include/stdafx.h
@@ -1,3 +1,29 @@
 #pragma once
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+// STL
 #include <print>
+#include <vector>
+#include <memory>
+#include <chrono>
+#include <numbers>
+#include <fstream>
+#include <filesystem>
+
+// VC++
+#include <windows.h>
+#include <comdef.h>
+#include <wrl/client.h>
+#include <wincodec.h>
+#include <dxgi1_6.h>
+
+// DirectX
+#include <d3d12.h>
+#include <d3d12sdklayers.h>
+#include <d3dx12.h>
+#include <DirectXMath.h>
+#include <DirectXMesh.h>
+#include <DirectXTex.h>
