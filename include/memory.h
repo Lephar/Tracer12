2cfd4037ce6b64306a769afb88fc56cae7c78ed0
Ali Emre Gülcü <aliemreglc@gmail.com>
2026-08-06 01:55:47 +0300

    Add heap class and implement allocation methods

--- include/memory.h
+++ include/memory.h
@@ -5,4 +5,5 @@
 #include "pch.h"
 
 namespace tracer::graphics::memory {
+	void allocate(Microsoft::WRL::ComPtr<ID3D12Device15> device, uint64_t buffersSize, uint64_t renderTargetDepthStencilTexturesSize, uint64_t nonRenderTargetDepthStencilTexturesSize);
 }
